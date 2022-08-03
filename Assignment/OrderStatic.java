b6f0e1c4bbe31df2d78da4503e7748b0d9e1fb5a
yashodareddy129 <110004725+yashodareddy129@users.noreply.github.com>
2022-08-03 11:23:19 +0530

    Add files via upload
    Programs added

---
+++ Assignment/OrderStatic.java
@@ -0,0 +1,53 @@
+package assistedPrmsArrays;
+class Smallst 
+{ 
+int Smallest(int arr[], int l, int r, int k) 
+    	{ 
+             		if (k > 0 && k <= r - l + 1) 
+        		{ 
+            			int pos = randomPartition(arr, l, r); 
+            			if (pos-l == k-1) 
+                			return arr[pos]; 
+            			if (pos-l > k-1) 
+                			return Smallest(arr, l, pos-1, k); 
+            			return Smallest(arr, pos+1, r, k-pos+l-1); 
+        		} 
+        return Integer.MAX_VALUE; 
+    } 
+    void swap(int arr[], int i, int j) 
+    { 
+        int temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
+    } 
+    int partition(int arr[], int l, int r) 
+    { 
+        int x = arr[r], i = l; 
+        for (int j = l; j <= r - 1; j++) 
+        { 
+            if (arr[j] <= x) 
+            { 
+                swap(arr, i, j); 
+                i++; 
+            } 
+        } 
+        swap(arr, i, r); 
+        return i; 
+    } 
+    int randomPartition(int arr[], int l, int r) 
+    { 
+        int n = r-l+1; 
+        int pivot = (int)(Math.random()) * (n-1); 
+        swap(arr, l + pivot, r); 
+        return partition(arr, l, r); 
+    } 
+}  
+public class OrderStatic
+{
+	public static void main(String[] args) {
+		Smallst ob = new Smallst(); 
+        int arr[] = {12, 3, 5, 7, 4, 19, 26}; 
+        int n = arr.length,k = 4; 
+        System.out.println("K'th smallest element is "+ ob.Smallest(arr, 0, n-1, k)); 
+    }
+}
